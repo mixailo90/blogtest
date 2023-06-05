3fb7781e641aa80ee97dd4606a51fb0e32cac0ba
mixailo90 <1990mixa@gmail.com>
2023-06-05 22:08:32 +0200

    Category service

---
+++ src/app/views/blog/services/blog-category.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { BlogCategoryRequest } from '../../../models/request/blog-category.request';
+import { BlogCategoryResponse } from '../../../models/response/blog-category.response';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BlogCategoryService {
+  constructor(private http: HttpClient) {}
+
+  public getAll(): Observable<BlogCategoryResponse[]> {
+    const url = `${environment.API_BASE_URL}/category`;
+    return this.http.get<BlogCategoryResponse[]>(url).pipe(map((response: any) => response.resultData));
+  }
+
+  public create(name: string): Observable<BlogCategoryResponse> {
+    const body: BlogCategoryRequest = new BlogCategoryRequest(name);
+    const url = `${environment.API_BASE_URL}/category`;
+    return this.http.post<BlogCategoryResponse>(url, body);
+  }
+}
